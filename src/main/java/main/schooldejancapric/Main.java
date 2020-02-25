93a4dd00b8bda7cfe73ab4e8614c2397d81ba16c
Dejan <capricdejan@gmail.com>
2020-02-25 20:20:46 +0100

    School

---
+++ src/main/java/main/schooldejancapric/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.schooldejancapric;
+
+import student.Student;
+/**
+ *
+ * @author qa
+ */
+public class Main {
+
+    /**
+     * @param args the command line argumentsYearOfBirth
+     */
+    public static void main(String[] args) {
+        
+        Student info = new Student("Petar", "Petrovic", 1990);
+//        info.setFirstName("Petar");
+ //       info.setLastName("Petrovic");
+  //      info.setYearOfBirth(1990);
+        
+        info.showData();
+                
+       
+    }
+    
+}
